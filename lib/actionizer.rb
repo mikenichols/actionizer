3c8f0375cf268248390ef5815e0d91a2e112990f
Mike Nichols <mykphyre@yahoo.com>
2016-02-28 13:43:54 -0600

    First test, calls #call when .call is invoked

--- lib/actionizer.rb
+++ lib/actionizer.rb
@@ -1,5 +1,15 @@
 require "actionizer/version"
 
 module Actionizer
-  # Your code goes here...
+  def self.included(base)
+    base.class_eval do
+      extend ClassMethods
+    end
+  end
+
+  module ClassMethods
+    def call
+      new.call
+    end
+  end
 end
--- spec/actionizer_spec.rb
+++ spec/actionizer_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe Actionizer do
-  it 'has a version number' do
-    expect(Actionizer::VERSION).not_to be nil
+  let(:dummy_class) do
+    Class.new do
+      include Actionizer
+
+      def call
+      end
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'has a version number' do
+    expect(Actionizer::VERSION).not_to be_nil
+  end
+
+  it 'invokes #call when you invoke .call' do
+    expect_any_instance_of(dummy_class).to receive(:call)
+    dummy_class.call
   end
 end
